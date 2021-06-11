7e506727226a48281e6c3608c03a0c1a5fe9149d
RUMAZUMD <RUMAZUMD@LBNL734795.mshome.net>
2021-06-11 15:30:01 +0200

    add updated generated code

--- StudentService/src/main/java/com/example/api/StudentApi.java
+++ StudentService/src/main/java/com/example/api/StudentApi.java
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.util.List;
 import java.util.Map;
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-24T21:17:51.734+01:00[Europe/Amsterdam]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-11T15:28:23.197742100+02:00[Europe/Amsterdam]")
 @Api(value = "student", description = "the student API")
 public interface StudentApi {
 
--- StudentService/src/main/java/com/example/models/Student.java
+++ StudentService/src/main/java/com/example/models/Student.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * Student
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-24T21:17:51.734+01:00[Europe/Amsterdam]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-11T15:28:23.197742100+02:00[Europe/Amsterdam]")
 public class Student   {
   @JsonProperty("StudentID")
   private Integer studentID = null;
