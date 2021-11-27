a15271cc8104dd61b1fc09c6a739feaa30af36dd
RUMAZUMD <RUMAZUMD@LBNL734795.mshome.net>
2021-11-27 22:31:20 +0100

    -

--- StudentService/src/main/java/com/example/models/StudentAddress.java
+++ StudentService/src/main/java/com/example/models/StudentAddress.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * StudentAddress
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-11-27T22:02:30.121+01:00[Europe/Berlin]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-11-27T22:29:20.324+01:00[Europe/Berlin]")
 public class StudentAddress   {
   @JsonProperty("HouseNumber")
   private Integer houseNumber = null;
